478be5c4f792e91824a8aae183919f1605fe4795
Amjad Yahia Roben Hassan <amjedyehia1998@gmail.com>
2022-02-02 11:56:17 +0200

    create addToCart Function + test realtes #25

--- logic.test.js
+++ logic.test.js
@@ -1,8 +1,4 @@
-const {
-    addToCart,
-    deleteFromCart,
-    getTotalPrice,
-} = require("./scripts/logic.js");
+const { addToCart } = require("./scripts/logic.js");
 
 const dummyCart = [
     { id: 1, title: "apple", price: 2 },
@@ -14,16 +10,5 @@ describe("cart crud operation", () => {
     test("should add product to cart", () => {
         addToCart(dummyCart, product);
         expect(dummyCart.at(-1)).toEqual(product);
-    });
-
-    test("should delete product from cart", () => {
-        deleteFromCart(dummyCart, 1);
-        expect(!dummyCart.includes(product)).toBeFalsy();
-    });
-
-    test("should sum the total price", () => {
-        const sum = getTotalPrice(dummyCart);
-
-        expect(sum).toEqual(14);
     });
 });
